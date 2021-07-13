b5e3a9b50daa74e721a4d630495d5a2f0f6765e9
GitHub Action <action@github.com>
2021-07-13 15:47:18 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nehapednekar/intellijmusic/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nehapednekar.intellijmusic.services
+
+import com.github.nehapednekar.intellijmusic.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nehapednekar/intellijmusic/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nehapednekar/intellijmusic/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nehapednekar.intellijmusic.services
 
+import com.github.nehapednekar.intellijmusic.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
